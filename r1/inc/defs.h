039e8b4ff97d9ad5d5de1e1551a2a42e26fccf39
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2025-04-15 10:54:45 +0300

    changed a comment

--- r1/inc/defs.h
+++ r1/inc/defs.h
@@ -5,9 +5,15 @@
     - PI    - the value of the constant pi
     - SIGN  - it influences what kind of definition is used for the
     first root of unity, which influences the order in which the 
-    roots appear
-    - TARGET_VALUE - the value that will be achieved for Nyquist after
-    the logarithmic normalization
+    roots appear. To be more explicit, some complex roots don't 
+    appear all by themselves, their conjugate (number with the 
+    same real part but opposite sign for the imaginary part)
+    appears also.
+    If C is (1, 2), conjugate of C is (1, -2). 
+    Changing the SIGN, than changes the definition, which 
+    switches the conjugates among themselves.
+    - TARGET_VALUE - is the value obtained for Nyquist after 
+    applying the logarithmic normalization
 */
 
 #ifndef _DEFS_H_
