deeb329dca4b773aef07e27a24066d47bc63d3ff
AVG <ardeleanu_vlad@ymail.com>
2025-10-08 13:51:08 +0300

    in one-dimensional 'main.c' I commented out the post-processing parts, maybe they could return

--- dimensions/1/src/main.c
+++ dimensions/1/src/main.c
@@ -2,7 +2,7 @@
 #include "fft_comp.h"
 #include "fft_prep_cpx.h"
 #include "fft_prep_bit.h"
-#include "fft_post_comp.h"
+/*#include "fft_post_comp.h"*/
 #include "defs.h"
 #include <stdlib.h>
 #include <math.h>
@@ -18,13 +18,13 @@ int main(){
   fft_order(LEN, PWR, VCT);
   fft_apply(LEN, PWR, VCT, RTS);
 
-  modulus_in_real(VCT, LEN);
-
-  LOG_SCALER = TARGET_VALUE/log(1+VCT[0]);//access the real part of the nyquist
-  
-  nyquist_arrange(VCT, LEN);
-
-  log_norm_real(VCT, LEN, LOG_SCALER);
+  /*modulus_in_real(VCT, LEN);*/
+  /**/
+  /*LOG_SCALER = TARGET_VALUE/log(1+VCT[0]);//access the real part of the nyquist*/
+  /**/
+  /*nyquist_arrange(VCT, LEN);*/
+  /**/
+  /*log_norm_real(VCT, LEN, LOG_SCALER);*/
   
   write("data/out", LEN, VCT);
 
